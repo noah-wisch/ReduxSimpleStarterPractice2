d6d7aeba924b755783be3434de016fe7cca48b30
Noah Wisch <noah.wisch@gmail.com>
2017-04-27 14:04:46 -0400

    created container

---
+++ src/reducers/ReducerBooks.js
@@ -0,0 +1,10 @@
+export default function() {
+  return (
+    [
+      {title: 'The Dog Stars'},
+      {title: 'The Buried Giant'},
+      {title: 'Harry Potter & The Deathly Hallows'},
+      {title: 'The Hunger Games: Catching Fire'},
+    ]
+  );
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './ReducerBooks';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BooksReducer
 });
 
 export default rootReducer;
